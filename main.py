18b4f9ea73a3ac3cff7615ed08821b951b50208e
雨巷 <43312224+chenyuxiang0425@users.noreply.github.com>
2020-08-02 22:25:38 +0800

    friendly output

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import roles
 
 def main():
     for _ in range(50):
-        print(simulation.redict_winner())
+        fight_times = 100000
+        winer_dict = simulation.redict_winner(fight_times)
+        for key, value in winer_dict.items():
+            print(key, "的胜率为", value/fight_times)
+
 
 if __name__ == '__main__':
     main()
