9fcf9104653210f6c48df65e04ad92d01652e769
雨巷 <43312224+chenyuxiang0425@users.noreply.github.com>
2020-08-03 00:55:18 +0800

    add 12x12 result output

--- main.py
+++ main.py
@@ -1,12 +1,54 @@
 import simulation
 import roles
+import pandas
+from pandas import DataFrame
 
 def main():
-    for _ in range(50):
-        fight_times = 100000
-        winer_dict = simulation.redict_winner(fight_times)
-        for key, value in winer_dict.items():
-            print(key, "的胜率为", value/fight_times)
+    role_list = {"Buronia": roles.Buronia,
+                 "Alin": roles.Alin,
+                 "YaYi": roles.YaYi,
+                 "YingLianZu": roles.YingLianZu,
+                 "DeLiSha": roles.DeLiSha,
+                 "JiZi": roles.JiZi,
+                 "XiEr": roles.XiEr,
+                 "DuYa": roles.DuYa,
+                 "LiTa": roles.LiTa,
+                 "Durandel": roles.Durandel,
+                 "FuHua": roles.FuHua,
+                 "Kiana": roles.Kiana
+    }
+
+    winning_probability = {"name": list(role_list.keys())}
+    for role, role_class in role_list.items():
+        winning_probability[role] = []
+
+    for attacker, attacker_class in role_list.items():
+        for defender, defender_class in role_list.items():
+            fight_times = 10000
+            if attacker == defender:
+                winning_probability[attacker].append(-1)
+            elif (attacker == 'LiTa' and defender == 'XiEr') or (attacker == 'XiEr' and defender == 'LiTa'):
+                winning_probability[attacker].append(-1)
+            else:
+                winner_dict = simulation.redict_winner(attacker_class, defender_class, fight_times)
+                winning_probability.get(attacker).append(winner_dict.get(attacker, 0)/fight_times)
+    print(winning_probability)
+    DataFrame(winning_probability).to_csv("C:\\Users\\cyx\\pythonProjects\\bbb_summerBet\\result.csv",index =False)
+
+
+def test_Alin_Jizi():
+
+    fight_times = 1000
+    winner_dict = simulation.redict_winner(roles.Alin, roles.JiZi, fight_times)
+    # print(winning_probability)
+    print(winner_dict)
+
+def test_LiTa_XiEr():
+
+    fight_times = 100
+    winner_dict = simulation.redict_winner(roles.LiTa, roles.XiEr, fight_times)
+    # print(winning_probability)
+    print(winner_dict)
 
 
 if __name__ == '__main__':
