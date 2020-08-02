ed60f923e6dbaef247743003384a5321d0da07c2
雨巷 <43312224+chenyuxiang0425@users.noreply.github.com>
2020-08-02 19:20:53 +0800

    modify main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import simulation
+import roles
+
 def main():
-    for _ in range(5):
-        print_redict_winner(roles.JiZi(), roles.DuYa())
+    for _ in range(50):
+        print(simulation.redict_winner())
 
 if __name__ == '__main__':
     main()
+
